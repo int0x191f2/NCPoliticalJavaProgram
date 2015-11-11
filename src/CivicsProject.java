2af054dd254233980a7d32f98751655183c85af5
Adam Long <macadam.long@gmail.com>
2015-11-11 21:31:18 +0000

    cleaned up, make cleaned up

--- src/CivicsProject.java
+++ src/CivicsProject.java
@@ -1,10 +1,32 @@
 import util.FileScanner;
 import util.Sorter;
 import java.util.ArrayList;
+import java.util.Collections;
 public class CivicsProject{
     public static void main(String [] args){
         Sorter repSorter=new Sorter("repList.txt");
+        Sorter countySorter=new Sorter("countyList.csv");
+
+        //Part 1
         System.out.println("Democrat Count: "+repSorter.search(1,"Democrat",0).size());
         System.out.println("Republican Count: "+repSorter.search(1,"Republican",0).size());
+        System.out.println("");
+
+        //Part 2
+        System.out.println("Democrat list in order:");
+        ArrayList<String> demoListSorted=repSorter.search(1,"Democrat",0);
+        Collections.sort(demoListSorted);
+        for(String rep : demoListSorted){
+            System.out.println(rep);
+        }
+        System.out.println("");
+
+        System.out.println("Republican list in order:");
+        ArrayList<String> repubListSorted=repSorter.search(1,"Republican",0);
+        Collections.sort(repubListSorted);
+        for(String rep : repubListSorted){
+            System.out.println(rep);
+        }
+        System.out.println("");
     }
 }
